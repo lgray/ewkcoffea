ecdfdda7ec6a813aeaca7b620d93397484010bc3
Kelci <k.mohrman@ufl.edu>
2023-12-01 23:10:32 -0500

    Split up run sh scripts

---
+++ analysis/wwz/full_run_futures.sh
@@ -0,0 +1,2 @@
+# Run at scale (with futures) DO NOT RUN THIS ON LOGIN NODE!
+time python run_wwz4l.py ../../input_samples/cfgs/wwz_analysis/mc_sig_bkg_samples.cfg,../../input_samples/cfgs/wwz_analysis/data_samples.cfg -o wwz_histos -x futures -n 128
